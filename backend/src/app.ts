46768725702efeada6c9ba71783a94e3c68aadf8
Mangalam13114 <mangalam13114@gmail.com>
2026-08-31 01:59:22 +0530

    feat: integrate Clerk authentication and set up user routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,23 +1,28 @@
-import express from "express"
+import express from "express";
+import { clerkMiddleware } from "@clerk/express";
 
-import authRoutes from "./routes/authRoutes"
-import chatRoutes from "./routes/chatRoutes"
-import messageRoutes from "./routes/messageRoutes"
-import userRoutes from "./routes/userRoutes"
+import authRoutes from "./routes/authRoutes";
+import chatRoutes from "./routes/chatRoutes";
+import messageRoutes from "./routes/messageRoutes";
+import userRoutes from "./routes/userRoutes";
+import { errorHandler } from "./middleware/errorHandler";
 
-const app = express()
+const app = express();
 
-app.use(express.json());   // parses incoming JSON request bodies and make them available as req.body in your route handlers
+app.use(express.json()); // parses incoming JSON request bodies and make them available as req.body in your route handlers
 
+app.use(clerkMiddleware());
 app.get("/health", (_req, res) => {
-    res.json({status: "OK", message: " server is running"});
+  res.json({ status: "OK", message: " server is running" });
 });
 
+app.use("/api/auth", authRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+app.use("/api/users", userRoutes);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/chat", chatRoutes)
-app.use("/api/message", messageRoutes)
-app.use("/api/users", userRoutes)
-
+// error handler must come after all the routes and other middlewares so they can catch errors passed with next(err)
+// or thrown inside async handlers
+app.use(errorHandler);
 
 export default app;
