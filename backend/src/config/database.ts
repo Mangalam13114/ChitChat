2d406b6a9aaa9453aa503a4c385502e61f8e5496
Mangalam13114 <mangalam13114@gmail.com>
2026-08-30 19:16:08 +0530

    feat: set up backend server with Express and MongoDB integration
    - Initialize server with Express and set up basic routing
    - Add MongoDB connection logic using Mongoose
    - Create models for Chat, Message, and User
    - Implement health check endpoint
    - Add scripts for development and production in package.json
    - Organize routes for authentication, chat, messages, and users

---
+++ backend/src/config/database.ts
@@ -0,0 +1,13 @@
+import mongoose, { mongo } from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI as string);
+        console.log("mongoDB connected successfully");
+    } catch (error) {
+        console.error("mongoDB connection error:", error);
+        process.exit(1);      // exit with failure
+        //  status code 1 means failure
+        //  status code 0 means success
+    }
+}
